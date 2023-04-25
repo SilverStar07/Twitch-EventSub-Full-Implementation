42465b5baa6c5332cd97696dadf0a9af7450ba0b
SilverStar07 <rosarc@gmail.com>
2023-04-25 11:59:14 -0500

    Redesigned EventSub subscription logic and updated README

--- src/bot.js
+++ src/bot.js
@@ -15,10 +15,11 @@ const chatbot = new tmi.client(configuration);
 chatbot.on("message", chatMessageHandler);
 chatbot.connect();
 
-const ngrokURL = "https://9ffb-2601-3c3-c200-2200-1-4d52-7193-ef59.ngrok.io";
+const ngrokURL = "";
 let access_token = "";
 
-const eventTypes = ["channel.update",
+const eventTypes = [
+    "channel.update",
     "channel.follow",
     "channel.subscribe",
     "channel.subscription.end",
@@ -54,7 +55,342 @@ const eventTypes = ["channel.update",
     "stream.offline",
     "user.authorization.grant",
     "user.authorization.revoke",
-    "user.update"];
+    "user.update",
+    "channel.charity_campaign.donate",
+    "channel.charity_campaign.start",
+    "channel.charity_campaign.progress",
+    "channel.charity_campaign.stop",
+    "channel.shield_mode.begin",
+    "channel.shield_mode.end",
+    "channel.shoutout.create",
+    "channel.shoutout.receive"
+];
+const eventTypesConfig = {
+    "channel.update": {
+        "type": "channel.update",
+        "version": "1",
+        "condition": {
+            "broadcaster_user_id": configuration.broadcaster_id
+        }
+    },
+    "channel.follow": {
+        "type": "channel.follow",
+        "version": "2",
+        "condition": {
+            "broadcaster_user_id": configuration.broadcaster_id,
+            "moderator_user_id": configuration.broadcaster_id
+        }
+    },
+    "channel.subscribe": {
+        "type": "channel.subscribe",
+        "version": "1",
+        "condition": {
+            "broadcaster_user_id": configuration.broadcaster_id
+        }
+    },
+    "channel.subscription.end": {
+        "type": "channel.subscription.end",
+        "version": "1",
+        "condition": {
+            "broadcaster_user_id": configuration.broadcaster_id
+        }
+    },
+    "channel.subscription.gift": {
+        "type": "channel.subscription.gift",
+        "version": "1",
+        "condition": {
+            "broadcaster_user_id": configuration.broadcaster_id
+        }
+    },
+    "channel.subscription.message": {
+        "type": "channel.subscription.message",
+        "version": "1",
+        "condition": {
+            "broadcaster_user_id": configuration.broadcaster_id
+        }
+    },
+    "channel.cheer": {
+        "type": "channel.cheer",
+        "version": "1",
+        "condition": {
+            "broadcaster_user_id": configuration.broadcaster_id
+        }
+    },
+    "channel.raid": {
+        "type": "channel.raid",
+        "version": "1",
+        "condition": {
+            "to_broadcaster_user_id": configuration.broadcaster_id
+        }
+    },
+    "channel.ban": {
+        "type": "channel.ban",
+        "version": "1",
+        "condition": {
+            "broadcaster_user_id": configuration.broadcaster_id
+        }
+    },
+    "channel.unban": {
+        "type": "channel.unban",
+        "version": "1",
+        "condition": {
+            "broadcaster_user_id": configuration.broadcaster_id
+        }
+    },
+    "channel.moderator.add": {
+        "type": "channel.moderator.add",
+        "version": "1",
+        "condition": {
+            "broadcaster_user_id": configuration.broadcaster_id
+        }
+    },
+    "channel.moderator.remove": {
+        "type": "channel.moderator.remove",
+        "version": "1",
+        "condition": {
+            "broadcaster_user_id": configuration.broadcaster_id
+        }
+    },
+    "channel.channel_points_custom_reward.add": {
+        "type": "channel.channel_points_custom_reward.add",
+        "version": "1",
+        "condition": {
+            "broadcaster_user_id": configuration.broadcaster_id
+        }
+    },
+    "channel.channel_points_custom_reward.update": {
+        "type": "channel.channel_points_custom_reward.update",
+        "version": "1",
+        "condition": {
+            "broadcaster_user_id": configuration.broadcaster_id
+        }
+    },
+    "channel.channel_points_custom_reward.remove": {
+        "type": "channel.channel_points_custom_reward.remove",
+        "version": "1",
+        "condition":
+            {
+                "broadcaster_user_id": configuration.broadcaster_id
+            }
+    },
+    "channel.channel_points_custom_reward_redemption.add": {
+        "type": "channel.channel_points_custom_reward_redemption.add",
+        "version": "1",
+        "condition":
+            {
+                "broadcaster_user_id": configuration.broadcaster_id
+            }
+    },
+    "channel.channel_points_custom_reward_redemption.update": {
+        "type": "channel.channel_points_custom_reward_redemption.update",
+        "version": "1",
+        "condition": {
+            "broadcaster_user_id": configuration.broadcaster_id
+        }
+    },
+    "channel.poll.begin": {
+        "type": "channel.poll.begin",
+        "version": "1",
+        "condition": {
+            "broadcaster_user_id": configuration.broadcaster_id
+        }
+    },
+    "channel.poll.progress": {
+        "type": "channel.poll.progress",
+        "version": "1",
+        "condition": {
+            "broadcaster_user_id": configuration.broadcaster_id
+        }
+    },
+    "channel.poll.end": {
+        "type": "channel.poll.end",
+        "version": "1",
+        "condition": {
+            "broadcaster_user_id": configuration.broadcaster_id
+        }
+    },
+    "channel.prediction.begin": {
+        "type": "channel.prediction.begin",
+        "version": "1",
+        "condition": {
+            "broadcaster_user_id": configuration.broadcaster_id
+        }
+    },
+    "channel.prediction.progress": {
+        "type": "channel.prediction.progress",
+        "version": "1",
+        "condition": {
+            "broadcaster_user_id": configuration.broadcaster_id
+        }
+    },
+    "channel.prediction.lock": {
+        "type": "channel.prediction.lock",
+        "version": "1",
+        "condition": {
+            "broadcaster_user_id": configuration.broadcaster_id
+        }
+    },
+    "channel.prediction.end": {
+        "type": "channel.prediction.end",
+        "version": "1",
+        "condition": {
+            "broadcaster_user_id": configuration.broadcaster_id
+        }
+    },
+    "drop.entitlement.grant": {
+        "type": "drop.entitlement.grant",
+        "version": "1",
+        "condition": {
+            "organization_id": configuration.broadcaster_id // Not sure if value is correct
+        },
+        "is_batching_enabled": true
+    },
+    "extension.bits_transaction.create": {
+        "type": "extension.bits_transaction.create",
+        "version": "1",
+        "condition": {
+            "extension_client_id": configuration.broadcaster_id // Not sure if value is correct
+        }
+    },
+    "channel.goal.begin": {
+        "type": "channel.goal.begin",
+        "version": "1",
+        "condition": {
+            "broadcaster_user_id": configuration.broadcaster_id
+        }
+    },
+    "channel.goal.progress": {
+        "type": "channel.goal.progress",
+        "version": "1",
+        "condition": {
+            "broadcaster_user_id": configuration.broadcaster_id
+        }
+    },
+    "channel.goal.end": {
+        "type": "channel.goal.end",
+        "version": "1",
+        "condition": {
+            "broadcaster_user_id": configuration.broadcaster_id
+        }
+    },
+    "channel.hype_train.begin": {
+        "type": "channel.hype_train.begin",
+        "version": "1",
+        "condition": {
+            "broadcaster_user_id": configuration.broadcaster_id
+        }
+    },
+    "channel.hype_train.progress": {
+        "type": "channel.hype_train.progress",
+        "version": "1",
+        "condition": {
+            "broadcaster_user_id": configuration.broadcaster_id
+        }
+    },
+    "channel.hype_train.end": {
+        "type": "channel.hype_train.end",
+        "version": "1",
+        "condition": {
+            "broadcaster_user_id": configuration.broadcaster_id
+        }
+    },
+    "stream.online": {
+        "type": "stream.online",
+        "version": "1",
+        "condition": {
+            "broadcaster_user_id": configuration.broadcaster_id
+        }
+    },
+    "stream.offline": {
+        "type": "stream.offline",
+        "version": "1",
+        "condition": {
+            "broadcaster_user_id": configuration.broadcaster_id
+        }
+    },
+    "user.authorization.grant": {
+        "type": "user.authorization.grant",
+        "version": "1",
+        "condition": {
+            "client_id": configuration.broadcaster_id // Not sure if value is correct
+        }
+    },
+    "user.authorization.revoke": {
+        "type": "user.authorization.revoke",
+        "version": "1",
+        "condition": {
+            "client_id": configuration.broadcaster_id // Not sure if value is correct
+        }
+    },
+    "user.update": {
+        "type": "user.update",
+        "version": "1",
+        "condition": {
+            "user_id": configuration.broadcaster_id
+        }
+    },
+    "channel.charity_campaign.donate": {
+        "type": "channel.charity_campaign.donate",
+        "version": "1",
+        "condition": {
+            "broadcaster_user_id": configuration.broadcaster_id
+        }
+    },
+    "channel.charity_campaign.start": {
+        "type": "channel.charity_campaign.start",
+        "version": "1",
+        "condition": {
+            "broadcaster_user_id": configuration.broadcaster_id
+        }
+    },
+    "channel.charity_campaign.progress": {
+        "type": "channel.charity_campaign.progress",
+        "version": "1",
+        "condition": {
+            "broadcaster_user_id": configuration.broadcaster_id
+        }
+    },
+    "channel.charity_campaign.stop": {
+        "type": "channel.charity_campaign.stop",
+        "version": "1",
+        "condition": {
+            "broadcaster_user_id": configuration.broadcaster_id
+        }
+    },
+    "channel.shield_mode.begin": {
+        "type": "channel.shield_mode.begin",
+        "version": "1",
+        "condition": {
+            "broadcaster_user_id": configuration.broadcaster_id,
+            "moderator_user_id": configuration.broadcaster_id
+        }
+    },
+    "channel.shield_mode.end": {
+        "type": "channel.shield_mode.end",
+        "version": "1",
+        "condition": {
+            "broadcaster_user_id": configuration.broadcaster_id,
+            "moderator_user_id": configuration.broadcaster_id
+        }
+    },
+    "channel.shoutout.create": {
+        "type": "channel.shoutout.create",
+        "version": "1",
+        "condition": {
+            "broadcaster_user_id": configuration.broadcaster_id,
+            "moderator_user_id": configuration.broadcaster_id
+        }
+    },
+    "channel.shoutout.receive": {
+        "type": "channel.shoutout.receive",
+        "version": "1",
+        "condition": {
+            "broadcaster_user_id": configuration.broadcaster_id,
+            "moderator_user_id": configuration.broadcaster_id
+        }
+    }
+};
+console.log("Number of events to subscribe to: " + eventTypes.length);
 
 axios.post("https://id.twitch.tv/oauth2/token" +
     "?client_id=" + configuration.API_KEY +
@@ -69,7 +405,9 @@ axios.post("https://id.twitch.tv/oauth2/token" +
     "moderator:manage:automod moderator:read:automod_settings moderator:manage:automod_settings " +
     "moderator:read:chat_settings moderator:manage:chat_settings user:edit user:edit:follows " +
     "user:manage:blocked_users user:read:blocked_users user:read:broadcast user:read:email user:read:follows " +
-    "user:read:subscriptions channel:moderate chat:edit chat:read whispers:read whispers:edit")
+    "user:read:subscriptions channel:moderate chat:edit chat:read whispers:read whispers:edit " +
+    "channel:read:charity moderator:read:shield_mode moderator:manage:shield_mode moderator:read:shoutouts " +
+    "moderator:manage:shoutouts moderator:read:followers")
     .then(response => {
         const responseData = response.data;
         access_token = responseData.access_token;
@@ -77,7 +415,7 @@ axios.post("https://id.twitch.tv/oauth2/token" +
         for (let i = 0; i < eventTypes.length; i++) {
             axios.post(ngrokURL + "/createWebhook?eventType=" + eventTypes[i])
                 .then(() => {
-                    console.log("Webhook successfully established");
+                    console.log(i, "Webhook successfully established: " + eventTypes[i]);
                 })
                 .catch(webhookError => {
                     console.log("Webhook creation error: " + webhookError);
@@ -118,6 +456,36 @@ const twitchWebhookEventHandler = (webhookEvent) => {
     // Do whatever crazy stuff you want to do with events here!
     // For information on individual event attributes go to
     // https://dev.twitch.tv/docs/eventsub/eventsub-subscription-types
+    const webhookSubscriptionType = webhookEvent.subscription.type;
+    let eventUsername = webhookEvent.event.user_name ? webhookEvent.event.user_name : '';
+
+    if (webhookSubscriptionType === "stream.online" || webhookSubscriptionType === "channel.update") {
+        setTimeout(() => {
+            // Put your code here to do something 30 seconds after the stream goes live,
+            // or you change the stream title or game.
+
+        }, 30000);
+    }
+    else if (webhookSubscriptionType === "channel.follow") {
+
+    }
+    else if (webhookSubscriptionType === "channel.subscribe") {
+
+    }
+    else if (webhookSubscriptionType === "channel.subscription.message") {
+        // This is the event for resubs
+    }
+    else if (webhookSubscriptionType === "channel.subscription.gift") {
+        const gifterName = webhookEvent.event.is_anonymous === true ? "Anonymous" : eventUsername;
+
+    }
+    else if (webhookSubscriptionType === "channel.cheer") {
+        const cheererName = webhookEvent.event.is_anonymous === true ? "Anonymous" : eventUsername;
+
+    }
+    else if (webhookSubscriptionType === "channel.raid") {
+
+    }
 };
 
 function chatMessageHandler(channel, userState, message, self) {
@@ -134,6 +502,7 @@ function chatMessageHandler(channel, userState, message, self) {
             .then(response => {
                 if (response.status === 200) {
                     const subscribedEvents = response.data;
+                    console.log("Number of events to unsubscribe: " + subscribedEvents.data.length);
 
                     for (let i = 0; i < subscribedEvents.data.length; i++) {
                         axios.delete("https://api.twitch.tv/helix/eventsub/subscriptions?id=" +
@@ -143,13 +512,11 @@ function chatMessageHandler(channel, userState, message, self) {
                                     "Client-ID": configuration.API_KEY,
                                     Authorization: "Bearer " + access_token
                                 }
-                            })
-                            .then(() => {
-                                console.log(subscribedEvents.data[i].type + " unsubscribed");
-                            })
-                            .catch(webhookError => {
-                                console.log("Webhook unsubscribe error: " + webhookError);
-                            });
+                            }).then(() => {
+                            console.log(i, subscribedEvents.data[i].type + " unsubscribed");
+                        }).catch(webhookError => {
+                            console.log("Webhook unsubscribe error: " + webhookError);
+                        });
                     }
                 }
                 else {
@@ -175,23 +542,23 @@ httpsApp.get('/redirect', function (request, response) {
 
 httpsApp.post('/twitchwebhooks/callback',
     async (request, response) => {
-    // Handle the Twitch webhook challenge
-    if (request.header("Twitch-EventSub-Message-Type") === "webhook_callback_verification") {
-        console.log("Verifying the Webhook is from Twitch");
-        response.writeHeader(200, {"Content-Type": "text/plain"});
-        response.write(request.body.challenge);
+        // Handle the Twitch webhook challenge
+        if (request.header("Twitch-EventSub-Message-Type") === "webhook_callback_verification") {
+            console.log("Verifying the Webhook is from Twitch");
+            response.writeHeader(200, {"Content-Type": "text/plain"});
+            response.write(request.body.challenge);
 
-        return response.end();
-    }
+            return response.end();
+        }
 
-    // Handle the Twitch event
-    const eventBody = request.body;
-    console.log("Recieving " +
-        eventBody.subscription.type + " request for " +
-        eventBody.event.broadcaster_user_name, eventBody);
-    twitchWebhookEventHandler(eventBody);
-    response.status(200).end();
-});
+        // Handle the Twitch event
+        const eventBody = request.body;
+        console.log("Recieving " +
+            eventBody.subscription.type + " request for " +
+            eventBody.event.broadcaster_user_name, eventBody);
+        twitchWebhookEventHandler(eventBody);
+        response.status(200).end();
+    });
 
 httpsApp.post('/createWebhook', (request, response) => {
     let createWebhookParameters = {
@@ -206,11 +573,7 @@ httpsApp.post('/createWebhook', (request, response) => {
     };
 
     let createWebhookBody = {
-        "type": request.query.eventType,
-        "version": "1",
-        "condition": {
-            "broadcaster_user_id": configuration.broadcaster_id,
-        },
+        ...eventTypesConfig[request.query.eventType],
         "transport": {
             "method": "webhook",
             "callback": ngrokURL + "/twitchwebhooks/callback",
